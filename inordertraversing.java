72765d4e9e41b5549bef04d627ed8b73b2c03a03
yash verma <44260505+vyash5075@users.noreply.github.com>
2019-08-09 11:34:00 +0530

    Create inordertraversing.java
    traversing of elements in inorder

---
+++ inordertraversing.java
@@ -0,0 +1,67 @@
+import java.util.*;
+class node
+        {
+    int data;
+    node left=null;
+    node right=null;
+}
+public class inordertraversing {
+    public static void main(String args[])
+    {
+        System.out.println("enter the no. of elements");
+        int n;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        node root=null;
+        node t=null;
+        node parent=null;
+        for(int i=0;i<n;i++)
+        {
+            node temp=new node();
+            temp.data=sc.nextInt();
+            if(root==null)
+            {
+                root=temp;
+            }
+            else{
+                t=root;
+                while(t!=null)
+                {
+                    parent=t;
+                    if(t.data>temp.data)
+                    {
+                        t=t.left;
+                    }
+                    else
+                    {
+                        t=t.right;
+                    }
+                    
+                }
+                if(parent.data>temp.data)
+                {
+                    parent.left=temp;
+                }
+                else
+                {
+                    
+                    parent.right=temp;
+                }
+            }
+                 
+        }
+        System.out.println("elements arrange in  inorder are");
+        postorder(root);
+    }
+    static void postorder(node root)
+    {
+        if(root!=null)
+        {
+            postorder(root.left);
+             System.out.println(root.data);
+            postorder(root.right);
+           
+        }
+    }
+            
+}
