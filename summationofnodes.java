ef45b7da12ecdd197d9189201f70e6ddd5e5bb4f
yash verma <44260505+vyash5075@users.noreply.github.com>
2019-08-20 23:50:18 +0530

    Create summationofnodes.java
    sum of all nodes present in the link list

---
+++ summationofnodes.java
@@ -0,0 +1,42 @@
+import java.util.*;
+class node{
+    int data;
+    Node address=null;
+}
+public class summationofnodes {
+    public static void main(String args[])
+    {
+        int n;
+        int sum=0;
+        Node start=null;
+        Scanner sc=new Scanner(System.in);
+       System.out.println("enter the no. of nodes");
+       n=sc.nextInt();
+       for(int i=0;i<n;i++)
+       {
+           Node temp=new Node();
+           temp.data=sc.nextInt();
+           if(start==null)
+           {
+               start=temp;
+           }
+           else{
+               temp.address=start;
+               start=temp;
+           }
+       }
+           Node ptr=start;
+           while(ptr!=null)
+           {
+               sum=sum+ptr.data;
+                ptr=ptr.address;
+                 
+           }
+            System.out.println( "sum is"+sum);
+           
+           
+       
+        
+    }
+    
+}
