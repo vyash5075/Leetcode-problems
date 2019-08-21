4bc63290084c4248fcd071516099288f8352645c
yash verma <44260505+vyash5075@users.noreply.github.com>
2019-08-21 23:37:38 +0530

    Create unionofNlinklists.java
    union of n  sorted nodes

---
+++ unionofNlinklists.java
@@ -0,0 +1,99 @@
+import java.util.*;
+import java.util.*;
+class Nodee{
+    int data;
+    Node address=null;
+}
+public class unionofNlinklists {
+    public static   void main(String args[])
+    {
+        int n;
+        int m;
+        int count=0;
+       // Node start=null;
+        Node start1=null;
+          Scanner sc=new Scanner(System.in);
+        System.out.println("enter the no of lists");
+        m=sc.nextInt(); 
+        System.out.println("enter the no of nodes");
+        
+      
+        n=sc.nextInt();
+        
+    for(int i=0;i<=m;i++)
+    {
+        System.out.println("enter elements of node "+count);
+     for(int j=0;j<n;j++)
+    {
+         Node temp1=new Node();
+        temp1.data=sc.nextInt();
+        temp1.address=null;
+        if(start1==null)
+        {
+            start1=temp1;
+        }
+        else
+        {
+            Node ptrr=start1;
+            Node prev=start1;
+            while(ptrr!=null&&ptrr.data<temp1.data)
+            {
+                 prev=ptrr;   
+                 ptrr=ptrr.address;
+            }
+                if(start1==ptrr)
+            {
+                temp1.address=start1;
+                start1=temp1; 
+            }   
+            else if(ptrr==null)
+            {
+                prev.address=temp1;
+            }
+             else
+            {
+                temp1.address=ptrr;
+                prev.address=temp1;
+            }
+        }
+    }
+    System.out.println("sorted list is given below");
+    Node ptr3=start1;
+    while(ptr3!=null)
+    {
+        System.out.println(ptr3.data);
+        ptr3=ptr3.address;
+    }
+    count++;
+    }
+        Node p=start1;
+        Node q;
+        while(p!=null&&p.address!=null)
+        {
+            if(p.data==p.address.data)
+            {
+                q=p.address.address;
+                if(q==null)
+                {
+                  p.address=null;
+                  break;
+                }
+                p.address=q;
+            }
+            if(p.data!=p.address.data)
+            {
+                p=p.address;
+            }
+        }
+         
+ System.out.println("union of given n nodes are");
+        Node ptr9=start1;
+        while(start1!=null)
+        {
+            System.out.println(ptr9.data);
+            ptr9=ptr9.address;
+        }
+    
+        
+    }
+}
