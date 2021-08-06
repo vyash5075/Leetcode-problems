e713007b2dd1ebba2e342fb5d435f6277d9b7264
Yash Verma <vyash5075@gmail.com>
2021-08-06 23:50:24 +0530

    Create N-ary Tree Level Order Traversal.java

---
+++ N-ary Tree Level Order Traversal.java
@@ -0,0 +1,23 @@
+  public List<List<Integer>> levelOrder(Node root) {
+            List<List<Integer>> result = new ArrayList<>();
+            if (root == null) {
+                return result;
+            }
+            Queue<Node> queue = new LinkedList<>();
+            queue.offer(root);
+            while (!queue.isEmpty()) {
+                int size = queue.size();
+                List<Integer> level = new ArrayList<>();
+                for (int i = 0; i < size; i++) {
+                    Node currentNode = queue.poll();
+                    if (currentNode != null) {
+                        level.add(currentNode.val);
+                        for (Node child : currentNode.children) {
+                            queue.offer(child);
+                        }
+                    }
+                }
+                result.add(level);
+            }
+            return result;
+        }
